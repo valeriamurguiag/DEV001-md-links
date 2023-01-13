7b0ebb1d2e1abd576b930e547cf89f52b7a77b75
Valeria Murguia <valeria.murguia.g@gmail.com>
2023-01-13 15:57:22 -0600

    Restructuré carpetas y rutas: src, test y primeros pasos

---
+++ primerosPasos/pathCheck.js
@@ -0,0 +1,13 @@
+// // Crear una función que cheque si la ruta ingresada es relativa o absoluta
+// const path = require('path');
+
+// const pathCheck = (pathFile) => {
+//     if(path.isAbsolute(pathFile)){
+//         console.log(pathFile);
+//     } else {
+//         const absolutePath = path.resolve(pathFile)
+//         console.log(absolutePath);
+//     }
+// }
+
+// pathCheck('./mdFiles/exampleFile.md')
---
+++ test/linksValid.spec.js
@@ -0,0 +1,32 @@
+const linksValid = require('../src/linksValid')
+
+// Test for getLinks
+describe('getLinks', () => {
+
+    const directoryPath = 'C:\\Users\\balry\\OneDrive\\Documentos\\Laboratoria\\Proyecto 4 - MD Links\\DEV001-md-links\\exampleFiles';  
+    const linksArr = [
+      {
+        href: 'https://es.wikipedia.org/wiki/Markdown',
+        text: 'Markdown',
+        file: 'C:\\Users\\balry\\OneDrive\\Documentos\\Laboratoria\\Proyecto 4 - MD Links\\DEV001-md-links\\exampleFiles\\exampleFile.md'
+      },
+      {
+        href: 'https://nodejs.org/en/',
+        text: 'Nodejs',
+        file: 'C:\\Users\\balry\\OneDrive\\Documentos\\Laboratoria\\Proyecto 4 - MD Links\\DEV001-md-links\\exampleFiles\\exampleFile.md'
+      },
+      {
+        href: 'https://postimg.cc/py9FKLgr',
+        text: 'Imagen final de proyecto',
+        file: 'C:\\Users\\balry\\OneDrive\\Documentos\\Laboratoria\\Proyecto 4 - MD Links\\DEV001-md-links\\exampleFiles\\exampleFile2.md'
+      }
+    ];
+  
+    it('should be a function', () => {
+      expect(typeof linksValid.getLinks).toBe('function');
+    }); 
+  
+    it('should return an array containing objects with links information', () => {
+      expect(linksValid.getLinks(directoryPath)).toEqual(linksArr);
+    });
+  });
